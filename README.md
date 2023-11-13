94f475e0f9491b07f938faef8f892dc10ee4dc95
Ma, Tzu-Yu <122843515+TzuYuMa@users.noreply.github.com>
2023-11-13 14:51:02 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# GIS5574_week10
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/TzuYuMa/pen/NWojJex](https://codepen.io/TzuYuMa/pen/NWojJex).
+
---
+++ dist/script.js
@@ -0,0 +1,20 @@
+require(["esri/views/MapView", "esri/WebMap", "esri/widgets/Directions"], (MapView, WebMap, Directions) => {
+  const webmap = new WebMap({
+    portalItem: {
+      id: "9afc43dab85349b595b815582609fbdc"
+    }
+  });
+
+  const view = new MapView({
+    map: webmap,
+    container: "viewDiv"
+  });
+  
+  const directions = new Directions({
+    view: view,
+    routeServiceUrl: "https://utility.arcgis.com/usrsvcs/appservices/7d0Q6PhsVvdlP0nO/rest/services/World/Route/NAServer/Route_World"
+    
+  });
+  view.ui.add(directions, "top-right");
+  
+});
